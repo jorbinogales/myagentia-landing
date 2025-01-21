33059bd93b8904b4a17d8f2015abd977b90ee93c
jorbinogales <jorbinogales@gmail.com>
2025-01-21 17:12:27 -0400

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: false, // Evita ignorar ESLint durante compilaciones (opcional)
-        rules: {
-          '@next/next/inline-script-id': 'off', // Desactiva la regla específica
-        },
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
